6bae7e317b0914c31d64d5c4f151a13c33ee68b9
Abdul Owhab <owhab.cse@gmail.com>
2023-05-15 15:35:56 +0600

    created user interface

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,36 @@ app.use(express.urlencoded());
 
 
 app.get('/', (req : Request, res : Response, next: NextFunction) => {
-    res.send('Hello Next Level Coders!')
-    next();
+    
+    /*
+    Steps to Inserting data to mongodp
+    Step- 1: Interface
+    Step- 2: Schema
+    Step- 3: Model
+    Step- 4: Database Query
+    */
+
+    // Creating an Interface
+    interface IUser{
+        id: string;
+        role: "student";
+        password: string;
+        name: {
+            firstName: string;
+            middleName: string;
+            lastName: string
+        };
+        dateOfBirth?: string;
+        gender: "Male" | "Female";
+        email?: string;
+        contactNo: string;
+        emergencyContact: string;
+        presentAddress: string
+    }
+    
+    
+    // res.send('Hello Next Level Coders!')
+    // next();
 
   
   })
