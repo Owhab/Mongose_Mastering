e105a69bcb3d772bbca6224cdbdf9ca5bfee3bb9
Abdul Owhab <owhab.cse@gmail.com>
2023-05-15 15:30:03 +0600

    added json parser and url encoded

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,18 @@ import cors from 'cors';
 const app : Application= express()
 app.use(cors());
 
+// Parsing Data
+app.use(express.json());
+
+// URL Encode
+app.use(express.urlencoded());
+
+
 
 app.get('/', (req : Request, res : Response, next: NextFunction) => {
     res.send('Hello Next Level Coders!')
     next();
-    
+
   
   })
 
